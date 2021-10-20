282581bf3081ea5b2ae4d608c9281983f94ef5a8
toni2110 <92384247+toni2110@users.noreply.github.com>
2021-10-20 12:57:22 +0200

    Update IAT-age_simple.js

--- IAT-age_simple.js
+++ IAT-age_simple.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/quiat8.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://toni2110.github.io/IAT-age/IAT-age-base.js'], function(APIConstructor, iatExtension){
     var API = new APIConstructor();
 var posWords = API.shuffle([
             'Kärlek', 'Glädje', 'Vän',  
